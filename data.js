75bbb8d16afeb81b4825a47027edf22d6be45cfd
zabdiel1298 <zabdiel_hiram@hotmail.com>
2020-11-16 21:43:07 -0700

    16-11-2020

---
+++ data.js
@@ -0,0 +1,43 @@
+module.exports = function() {
+    return {
+        products: [{
+                id: 1,
+                name: "kayak",
+                category: "Deportes acuáticos",
+                description: "un bote por persona",
+                price: 275
+            },
+            {
+                id: 2,
+                name: "chaleco salvavidas",
+                category: "deportes acuáticos",
+                description: "Protector",
+                price: "48.95"
+            },
+            {
+                id: 3,
+                name: "balón de futbol",
+                category: "futbol",
+                description: "tamaño y peso aprobados por la FIFA",
+                price: 19.50
+            },
+            {
+                id: 4,
+                name: "banderas de esquina",
+                category: "futbol",
+                description: "dale a tu campo de juego un toque profesional",
+                price: 34.95
+            },
+            {
+                id: 5,
+                name: "Estadio",
+                category: "Futbol",
+                description: "Estadio plano de 35,000 asientos",
+                price: 79500
+            },
+
+
+        ],
+        orders: []
+    }
+}
